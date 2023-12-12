ec625058e58ee4f59808c5596632e1bd5e22ae16
Ateo <79461147+ateodev@users.noreply.github.com>
2023-12-12 14:53:10 +0100

    upload unity test build

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const app = new Application();
 
-app.use(staticFiles("public"));
+app.use(async (context, next) => {
+  try {
+    await context.send({
+      root: `${Deno.cwd()}/public`,
+      index: "index.html",
+    });
+  } catch {
+    await next();
+  }
+});
 
+console.log(`Server running http://localhost:3000`)
 await app.listen({ port: 3000 });
